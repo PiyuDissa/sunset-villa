97f0a6321b54c4c7e23389de231435bfc46953a9
Piyumika Dissanayake <piyumika.swd@gmail.com>
2025-09-12 15:05:28 +0530

    font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { inter } from '@/app/ui/fonts';
 import Header from "./ui/header";
 
 const geistSans = Geist({
@@ -25,9 +26,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${inter.className} antialiased`}>
         <Header />
         {children}
       </body>
